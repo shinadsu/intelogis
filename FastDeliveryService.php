72d7e7a200ff2f0a6d360baf6b921ff3962a705f
shinadsu <69975772+shinadsu@users.noreply.github.com>
2023-09-22 13:24:58 +0300

    initial

---
+++ FastDeliveryService.php
@@ -0,0 +1,5 @@
+class FastDeliveryService extends DeliveryService {
+    public function calculateCostAndDate($sourceKladr, $targetKladr, $weight) {
+        // Реализация расчета стоимости и сроков для быстрой доставки
+    }
+}
