c4cc94631769487400e7ad4b1c19bedc2006c9e1
shinadsu <69975772+shinadsu@users.noreply.github.com>
2023-09-22 13:25:39 +0300

    initial

---
+++ SlowDeliveryService.php
@@ -0,0 +1,10 @@
+
+<?php
+
+class SlowDeliveryService extends DeliveryService {
+    public function calculateCostAndDate($sourceKladr, $targetKladr, $weight) {
+        // Реализация расчета стоимости и сроков для медленной доставки
+    }
+}
+
+?>
